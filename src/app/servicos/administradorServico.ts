790fb1f66745763426ae963e875eb30e5786a21e
João Paulo Ghissardi da Silva <jp17paulo@terra.com.br>
2022-10-04 17:12:15 -0300

    Testes relacionados ao token

--- src/app/servicos/administradorServico.ts
+++ src/app/servicos/administradorServico.ts
@@ -10,7 +10,8 @@ export class AdministradorServico{
 
   public async todos(pagina:Number) {
     // return await this.http.get<Paginacao>(`${environment.administradoresAPI}/administradores?page=${pagina}`, { headers: new HttpHeaders({authorization: `Bearer ${LocalServico.getToken()}`})}).toPromise()
-    return await lastValueFrom(this.http.get<Paginacao>("https://localhost:5003/administradores"))
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Ikpvw6NvIFBhdWxvIEdoaXNzYXJkaSBkYSBTaWx2YSIsInJvbGUiOiJhZG1pbmlzdHJhZG9yIiwibmJmIjoxNjY0OTE0MDQwLCJleHAiOjE2NjQ5MjQ4NDAsImlhdCI6MTY2NDkxNDA0MH0.vdRmGRzsWrqZjA2qWoAsw1M7tRI_VZ5W9U_pTqQzns4"
+    return await lastValueFrom(this.http.get<Paginacao>('https://localhost:5003/administradores' , { headers: new HttpHeaders({authorization: `Bearer ${token}`})}))
   }
 
   public async logar(administrador:Administrador) {
